5f949c4df2e5edbdf5e78d70acd15df4daef8ede
Doniyor11 <udoniyorbek99@mail.ru>
2024-12-04 18:05:54 +0500

    3.12.24

---
+++ src/widgets/popular-packages/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@mantine/core"
+import React from "react"
+
+import s from "./popular-packages.module.scss"
+
+export const PopularPackages = () => {
+	return (
+		<>
+			<Text component={"h2"}>Popular tour packages</Text>
+		</>
+	)
+}
